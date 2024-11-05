ffabfd3c612ff16814973d74a01584fc3e998d73
Muhammad Umar <umar.throv@gmail.com>
2024-11-05 19:38:13 +0500

    update

--- config/wsgi.py
+++ config/wsgi.py
@@ -3,4 +3,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# Create the WSGI application
 application = get_wsgi_application()
+
+# Vercel expects the handler to be named 'app'
+app = application

5714f5bfd6766d6451992ef4567bbe835bfc9063
Muhammad Umar <umar.throv@gmail.com>
2024-11-06 19:59:14 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
 from django.views.static import serve
 
 

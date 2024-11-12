272bc42c0e88313e8143b6559da92c42aed8f23b
Muhammad Umar <118300490+toiroff@users.noreply.github.com>
2024-11-12 11:04:42 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -263,10 +263,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static') ]
-STATIC_ROOT = BASE_DIR / 'staticfiles' / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT =   BASE_DIR / '/tmp/media/'
+MEDIA_ROOT =   BASE_DIR / '/media/'
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
